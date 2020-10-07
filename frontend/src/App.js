0817b7849be5ffcb856fa458f107cefaafa4f61c
Eric Phillips <e.phillips.59715@gmail.com>
2020-10-07 11:40:14 -0700

    Fixed Navbar issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./styles/App.css"
 import HomePage from "./components/pages/HomePage";
 
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import NewNavbar from "./components/NewNavbar";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
-        <NewNavbar />
+        <Navbar />
         <Switch>
           <Route path="/" exact component={HomePage} />
         </Switch>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,49 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCoffee, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCoffee,
+  faShoppingCart,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/Navbar.css";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
-
   const handleClick = () => setClicked(!clicked);
+
+  useEffect(() => {
+    if (clicked) {
+      document.querySelector(".sidebar").classList.add("open");
+    } else {
+      document.querySelector(".sidebar").classList.remove("open");
+    }
+  }, [clicked]);
 
   return (
     <React.Fragment>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            <FontAwesomeIcon icon={faCoffee} />
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
+          <div className="nav-icon" onClick={handleClick}>
             <FontAwesomeIcon icon={clicked ? faTimes : faBars} />
           </div>
-          <ul className={clicked ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                About Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Locations
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/store" className="nav-links">
-                Store
-              </Link>
-            </li>
-          </ul>
+          <div className="nav-icon">
+            <Link to="/">
+              <FontAwesomeIcon icon={faCoffee} />
+            </Link>
+          </div>
+          <div className="nav-icon">
+            <Link to="/cart">
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </Link>
+          </div>
         </div>
       </nav>
+      <aside className="sidebar">
+        <h3>Pages</h3>
+        <ul>
+          <li>
+            <Link to="/" onClick={handleClick}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/store" onClick={handleClick}>
+              Store
+            </Link>
+          </li>
+        </ul>
+      </aside>
     </React.Fragment>
   );
 };
